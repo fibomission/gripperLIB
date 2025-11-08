944173db9853a71dc9440fce104cd22539a6ab9c
Tian <tiantian067675@gmail.com>
2025-11-09 01:39:25 +0700

    Update gripperlib.cpp

--- gripperlib.cpp
+++ gripperlib.cpp
@@ -1,22 +1,31 @@
 #include "gripperlib.hpp"
 
-      void gripper::gripper_open(){
-        if (is_inverted==false){
-        gripper_servo.write(max);
-        }
-        else{
-          gripper_servo.write(min);
-        }
-      }
-      void gripper::gripper_close(){
-        if (is_inverted==false){
-        gripper_servo.write(min);
-        }
-        else{
-          gripper_servo.write(max);
-        }
-      }
-      gripper::gripper(byte pin){
-        gripper_servo.attach(pin);
-      }
-
+void gripper::gripper_open()
+{
+  if (is_inverted == false)
+  {
+    gripper_servo->write(max_angle);
+  }
+  else
+  {
+    gripper_servo->write(min_angle);
+  }
+}
+void gripper::gripper_close()
+{
+  if (is_inverted == false)
+  {
+    gripper_servo->write(min_angle);
+  }
+  else
+  {
+    gripper_servo->write(max_angle);
+  }
+}
+gripper::gripper(Servo*servo, int min_angle, int max_angle, bool is_inverted)
+{
+  this->gripper_servo=servo;
+  this->max_angle = max_angle;
+  this->min_angle = min_angle;
+  this->is_inverted = is_inverted;
+}
