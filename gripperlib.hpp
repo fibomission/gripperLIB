fe7e0b85382b918fb630f4601b619b38d6c91db3
Tian <tiantian067675@gmail.com>
2025-11-09 01:41:06 +0700

    Update gripperlib.hpp

--- gripperlib.hpp
+++ gripperlib.hpp
@@ -1,30 +1,22 @@
 #pragma once
-#include<Arduino.h>
-#include<ESP32Servo.h>
+#include <Arduino.h>
+#include <ESP32Servo.h>
 
 #ifndef invert
 #define invert false
 #endif
 
-#ifndef max_angle
-#define max_angle 90
-#endif
+class gripper
+{
 
-#ifndef min_angle
-#define min_angle 0
-#endif
+public:
+    gripper(Servo*servo,int min_angle, int max_angle, bool is_inverted);
+    void gripper_open();
+    void gripper_close();
 
-    class gripper{
-        
-        public:
-        static constexpr uint8_t max= static_cast<uint8_t>(max_angle);
-        static constexpr uint8_t min= static_cast<uint8_t>(min_angle);
-        static constexpr bool is_inverted=invert;
-        void gripper_open();
-        void gripper_close();
-        gripper(byte pin);
-        private:
-
-            Servo gripper_servo;
-    };
-    
+private:
+    Servo*gripper_servo;
+    int max_angle;
+    int min_angle;
+    bool is_inverted;
+};
